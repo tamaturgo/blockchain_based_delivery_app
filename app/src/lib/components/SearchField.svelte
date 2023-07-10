0229532582c9b68676bad027c8a8d77184a90696
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-10 17:19:45 -0400

    Created search field component

--- app/src/lib/components/SearchField.svelte
+++ app/src/lib/components/SearchField.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+    import {getContext} from 'svelte'
+
     const placeholder:string = 'Pesquisar encomenda'
-    let field:any
+    let field : any
+    let value : string = ''
+
+    const { searchDelivery } = getContext('home')
+
+    function handleSearch(e : KeyboardEvent) {
+        if (e.key == 'Enter' && value != '') {
+            searchDelivery(value)
+        }
+    }
+
 </script>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<form on:click={() => field.focus()} class='w-80 h-16 p-4 gap-4 bg-[#FFFFFF] rounded-xl flex items-center hover:cursor-text'>
-    <img src="/src/assets/Search.svg" alt="Search icon"/>
-    <input placeholder={placeholder} bind:this={field} class="border-none outline-none w-full h-full bg-[#FFF] text-black-text caret-blue-primary"/>
-</form>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:click={() => field.focus()} class='w-80 h-16 p-4 gap-4 bg-[#FFFFFF] rounded-xl flex items-center hover:cursor-text'>
+    <img src="/src/assets/Search.svg" alt="Search icon" on:click={() => searchDelivery(field)}/>
+    <input placeholder={placeholder} bind:this={field} bind:value={value} on:keypress={handleSearch} class="border-none outline-none w-full h-full bg-[#FFF] text-black-text caret-blue-primary"/>
+</div>
+
