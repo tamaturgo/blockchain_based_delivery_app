e4c248b5d8acbdc5d2272a351b1eb29f50563afe
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-10 12:57:07 -0400

    Search field base component

---
+++ app/src/lib/components/SearchField.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    const placeholder:string = 'Pesquisar encomenda'
+    let field:any
+</script>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<form on:click={() => field.focus()} class='w-80 h-16 p-4 gap-4 bg-[#FFFFFF] rounded-xl flex items-center hover:cursor-text'>
+    <img src="/src/assets/Search.svg" alt="Search icon"/>
+    <input placeholder={placeholder} bind:this={field} class="border-none outline-none w-full h-full bg-[#FFF] text-black-text caret-blue-primary"/>
+</form>
