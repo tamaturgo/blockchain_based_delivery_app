46644ab9de776de8cb06e7bb90ae2bced9a9f9ea
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-10 17:20:17 -0400

    Created add card component

---
+++ app/src/lib/components/AddCard.svelte
@@ -0,0 +1,23 @@
+<main class="w-full rounded-lg bg-[#FFF] p-6 flex flex-col items-start gap-4 my-6">
+    <h1 class="font-semibold text-xl">Encomenda</h1>
+    <div class="flex flex-col items-start gap-2 w-full">
+        <div class="text-sm">ID do produto:</div>
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+    </div>
+    <div class="flex flex-col items-start gap-2 w-full">
+        <div class="text-sm">ID do produto:</div>
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+    </div>
+    <div class="flex flex-col items-start gap-2 w-full">
+        <div class="text-sm">ID do produto:</div>
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+    </div>
+    <div class="flex flex-col items-start gap-2 w-full">
+        <div class="text-sm">ID do produto:</div>
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+    </div>
+    <div class="flex flex-col items-start gap-2 w-full">
+        <div class="text-sm">ID do produto:</div>
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+    </div>
+</main>
