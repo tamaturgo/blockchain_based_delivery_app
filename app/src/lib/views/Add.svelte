500019a2c94e2820d70ef6e7299d3f00f0a29974
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-11 02:30:41 -0400

    Add delivery view

---
+++ app/src/lib/views/Add.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { link } from "svelte-spa-router";
+    import AddCard from "../components/AddCard.svelte";
+</script>
+
+<main class="mt-40 w-80 flex flex-col items-center">
+    <a
+        use:link={"/"}
+        class="text-sm self-start cursor-pointer hover:font-semibold"
+        >Cancelar</a
+    >
+    <img src="/src/assets/CRYPTOpost.svg" alt="Logo" class="my-6" />
+    <AddCard />
+    <div class="cursor-pointer hover:font-semibold self-end mt-6">
+        Adicionar
+    </div>
+</main>
