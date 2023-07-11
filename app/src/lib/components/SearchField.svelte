87e45822c7bb5885a91251c288c7b784250a248f
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-11 02:28:02 -0400

    Creating search field

--- app/src/lib/components/SearchField.svelte
+++ app/src/lib/components/SearchField.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-    import {getContext} from 'svelte'
+    import { push } from "svelte-spa-router";
+    const placeholder: string = "Pesquisar encomenda";
+    let field: any;
+    let value: string = "";
 
-    const placeholder:string = 'Pesquisar encomenda'
-    let field : any
-    let value : string = ''
-
-    const { searchDelivery } = getContext('home')
-
-    function handleSearch(e : KeyboardEvent) {
-        if (e.key == 'Enter' && value != '') {
-            searchDelivery(value)
+    function handleSearch(e: KeyboardEvent) {
+        if (e.key == "Enter") {
+            push("/search/" + value);
         }
     }
-
 </script>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<div on:click={() => field.focus()} class='w-80 h-16 p-4 gap-4 bg-[#FFFFFF] rounded-xl flex items-center hover:cursor-text'>
-    <img src="/src/assets/Search.svg" alt="Search icon" on:click={() => searchDelivery(field)}/>
-    <input placeholder={placeholder} bind:this={field} bind:value={value} on:keypress={handleSearch} class="border-none outline-none w-full h-full bg-[#FFF] text-black-text caret-blue-primary"/>
-</div>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+    on:click={() => field.focus()}
+    class="w-full h-16 p-4 gap-4 bg-[#FFFFFF] rounded-lg flex items-center hover:cursor-text"
+>
+    <img
+        src="/src/assets/Search.svg"
+        on:keypress={handleSearch}
+        alt="Search icon"
+    />
+    <input
+        {placeholder}
+        bind:this={field}
+        bind:value
+        on:keypress={handleSearch}
+        class="border-none outline-none w-full h-full bg-[#FFF] text-black-text caret-blue-primary"
+    />
+</div>
