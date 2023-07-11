3bc6fb958fb43a4c623dfe47a9049c643a29c225
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-11 02:31:38 -0400

    Not found view

---
+++ app/src/lib/views/NotFound.svelte
@@ -0,0 +1,20 @@
+<script>
+    import SearchCard from "../components/SearchCard.svelte";
+    import SearchField from "../components/SearchField.svelte";
+    import { link } from "svelte-spa-router";
+
+    export let params = {};
+</script>
+
+<body class="w-80 flex flex-col mt-40">
+    <a
+        use:link
+        href="/add"
+        class="text-sm self-start cursor-pointer hover:font-semibold"
+        >Adicionar</a
+    >
+    <img src="/src/assets/CRYPTOpost.svg" alt="Logo" class="my-6" />
+    <SearchField />
+    <div class="text-9xl mt-20 mb-14 opacity-50">:(</div>
+    <div class="opacity-50">Encomenda não encontrada</div>
+</body>
