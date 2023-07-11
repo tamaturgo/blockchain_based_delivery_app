66a59c6ce743717ae3ec104e81435ea75117a79a
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-11 02:30:24 -0400

    Created edit delivery card
    Enables user to update the state of a delivery

---
+++ app/src/lib/components/EditCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let id: string;
+</script>
+
+<main>
+    <div
+        class="flex flex-col w-80 p-6 my-6 gap-6 items-start rounded-xl text-black-text bg-[#FFF]"
+    >
+        <h1 class="text-xl font-semibold">Encomenda</h1>
+        <div class="flex flex-col items-start h-10 gap-2">
+            <h2 class="font-semibold text-sm">ID do produto</h2>
+            {id}
+        </div>
+        <input
+            placeholder="Novo status"
+            class="border-background border-2 rounded-lg p-4 outline-none w-full h-full bg-[#FFF] text-black-text caret-blue-primary"
+        />
+    </div>
+</main>
