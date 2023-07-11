79add94384317652e07b82574f9476fb15d808a3
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-11 02:27:37 -0400

    Migrating router package
    From svelte-router to svelte-spa-router

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,12 +1,21 @@
 <script>
-import {Router, Route} from 'svelte-routing'
-import Home from './lib/views/Home.svelte'
+import Router from 'svelte-spa-router'
+    import Home from './lib/views/Home.svelte'
+import Add from './lib/views/Add.svelte';
+    import SearchField from './lib/components/SearchField.svelte';
+    import Search from './lib/views/Search.svelte';
+    import Edit from './lib/views/Edit.svelte';
+    import NotFound from './lib/views/NotFound.svelte';
 
-export let url = '/'
+const routes = {
+    '/': Home,
+    '/add': Add,
+    '/search/:id': Search,
+    '/edit/:id': Edit,
+    '*': NotFound
+}
 </script>
 
-<Router {url}>
-    <div>
-        <Route path='/'><Home/></Route>
-    </div>
-</Router>
+<main class="min-h-screen w-screen bg-background flex flex-col items-center gap-10 text-black-text">
+    <Router {routes}/>
+</main>
