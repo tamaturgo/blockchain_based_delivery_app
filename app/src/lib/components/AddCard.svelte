9ec60e5059717acb0bef38b4deed06e35dc96218
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-11 02:29:30 -0400

    Creating add delivery card
    Enables user to enter information about a new delivery

--- app/src/lib/components/AddCard.svelte
+++ app/src/lib/components/AddCard.svelte
@@ -1,23 +1,16 @@
-<main class="w-full rounded-lg bg-[#FFF] p-6 flex flex-col items-start gap-4 my-6">
+<main class="w-full rounded-lg bg-[#FFF] p-6 flex flex-col items-start gap-4">
     <h1 class="font-semibold text-xl">Encomenda</h1>
     <div class="flex flex-col items-start gap-2 w-full">
-        <div class="text-sm">ID do produto:</div>
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+        <div class="text-sm">Nome do produto:</div>
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="Nome do produto" />
     </div>
     <div class="flex flex-col items-start gap-2 w-full">
-        <div class="text-sm">ID do produto:</div>
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do remetente" />
     </div>
     <div class="flex flex-col items-start gap-2 w-full">
-        <div class="text-sm">ID do produto:</div>
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do destinatário" />
     </div>
     <div class="flex flex-col items-start gap-2 w-full">
-        <div class="text-sm">ID do produto:</div>
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
-    </div>
-    <div class="flex flex-col items-start gap-2 w-full">
-        <div class="text-sm">ID do produto:</div>
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="Quantia" />
     </div>
 </main>
