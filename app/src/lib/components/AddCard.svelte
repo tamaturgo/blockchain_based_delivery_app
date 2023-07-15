8447aecc8dabb9ed9ac2bd2060db21546de23676
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-15 15:49:30 -0400

    Add view post request and form validation

--- app/src/lib/components/AddCard.svelte
+++ app/src/lib/components/AddCard.svelte
@@ -1,16 +1,12 @@
+<script lang="ts">
+    export let delivery;
+
+</script>
 <main class="w-full rounded-lg bg-[#FFF] p-6 flex flex-col items-start gap-4">
     <h1 class="font-semibold text-xl">Encomenda</h1>
-    <div class="flex flex-col items-start gap-2 w-full">
-        <div class="text-sm">Nome do produto:</div>
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="Nome do produto" />
-    </div>
-    <div class="flex flex-col items-start gap-2 w-full">
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do remetente" />
-    </div>
-    <div class="flex flex-col items-start gap-2 w-full">
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do destinatário" />
-    </div>
-    <div class="flex flex-col items-start gap-2 w-full">
-        <input class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="Quantia" />
-    </div>
+        <input required bind:value={delivery.product} class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="Nome do produto" />
+        <input required bind:value={delivery.product_id} class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do produto" />
+        <input required bind:value={delivery.sender} class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do remetente" />
+        <input required bind:value={delivery.receiver} class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="ID do destinatário" />
+        <input required min="1" max="1000" bind:value={delivery.amount} type="number" class="outline-none py-3 px-4 border-2 border-background rounded-lg bg-[#FFF] w-full" placeholder="Quantia" />
 </main>
--- app/src/lib/views/Add.svelte
+++ app/src/lib/views/Add.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
     import { link } from "svelte-spa-router";
     import AddCard from "../components/AddCard.svelte";
+    import Logo from "../components/Logo.svelte";
+
+    let delivery = {
+        product: '',
+        product_id: '',
+        sender: '',
+        receiver: '',
+        amount: 0
+    }
+    
+    async function addDelivery() {
+        if (
+            Object.entries(delivery).every(
+                (value) => value != null && value != undefined
+            )
+        ) {
+            await fetch("http://localhost:5000/delivery", {
+                method: 'POST',
+                body: JSON.stringify(delivery),
+            }).then((res) => console.log(res.body));
+        }
+    }
 </script>
 
 <main class="mt-40 w-80 flex flex-col items-center">
@@ -10,9 +32,12 @@
         class="text-sm self-start cursor-pointer hover:font-semibold"
         >Cancelar</a
     >
-    <img src="/src/assets/CRYPTOpost.svg" alt="Logo" class="my-6" />
-    <AddCard />
-    <div class="cursor-pointer hover:font-semibold self-end mt-6">
-        Adicionar
-    </div>
+    <Logo/>
+    <form name="add" class="w-full flex flex-col" on:submit|preventDefault={addDelivery}>
+    <AddCard {delivery} />
+    <input
+        type="submit"
+        value="Adicionar"
+        class="cursor-pointer hover:font-semibold self-end mt-6"/>
+    </form>
 </main>
