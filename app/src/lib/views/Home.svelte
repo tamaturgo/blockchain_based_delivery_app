9ecd9abe82c1be7197f5f883ed65e310321a0d86
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-16 17:41:10 -0400

    Refactoring Home logic

--- app/src/lib/views/Home.svelte
+++ app/src/lib/views/Home.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
-    import {link} from 'svelte-spa-router'
+    import { link } from "svelte-spa-router";
     import SearchField from "../components/SearchField.svelte";
     import { setContext } from "svelte";
+    import Logo from "../components/Logo.svelte";
 
     let delivery = {
-        'id': ''
-    }
-    
-    setContext('home', { searchDelivery})
+        id: "",
+    };
 
-    function searchDelivery(id : string) {
-        delivery['id'] = id
+    setContext("home", { searchDelivery });
+
+    function searchDelivery(id: string) {
+        delivery["id"] = id;
     }
 </script>
 
-<body class="w-80 flex flex-col mt-40">
-    <a href="/add" use:link class="text-sm self-start cursor-pointer hover:font-semibold">Adicionar</a>
-    <img src="/src/assets/CRYPTOpost.svg" alt="Logo" class="my-6"/>
-    <SearchField/>
-</body>
-
+<main class="w-80 flex flex-col mt-40 items-center">
+    <a
+        href="/add"
+        use:link
+        class="text-sm self-start cursor-pointer hover:font-semibold"
+        >Adicionar</a
+    >
+    <Logo />
+    <SearchField />
+</main>
