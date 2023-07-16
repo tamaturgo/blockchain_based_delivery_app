1401885f9723ba71259014506aa850a9c10fd1b5
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-16 18:46:48 -0400

    Styling search card

--- app/src/lib/components/SearchCard.svelte
+++ app/src/lib/components/SearchCard.svelte
@@ -7,9 +7,8 @@
 <main
     class="flex flex-col w-80 p-6 my-6 gap-6 items-start rounded-xl text-black-text bg-[#FFF]"
 >
-    <h1 class="text-xl font-semibold">Encomenda</h1>
+    <div class="text-xl font-semibold">Encomenda</div>
 
-    <div class="flex justify-between" />
     <div class="flex flex-col items-start h-10 gap-2">
         <h2 class="font-semibold text-sm">ID do produto</h2>
         {id}
@@ -17,11 +16,11 @@
 
     <div class="flex flex-col items-start h-10 gap-2">
         <h2 class="font-semibold text-sm">Nome do produto</h2>
-        {name}
+        <div class="capitalize">{name}</div>
     </div>
 
-    <h2 class="font-semibold text-sm">Status</h2>
     <dl class="flex flex-col list-disc items-start">
+    <h2 class="font-semibold text-sm">Status</h2>
     {#each status as element}
             <dt class="capitalize">{element[0]}</dt>
             <dd class="text-sm">{new Date(element[1]).toLocaleString()}</dd>
