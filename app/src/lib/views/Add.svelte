6cadace910ac4c8d0574091d08b0980d559bf656
Luís Henrique Scantelbury de Almeida <83627038+lscantelbury@users.noreply.github.com>
2023-07-16 21:29:59 -0400

    Migrating add delivery request to axios

--- app/src/lib/views/Add.svelte
+++ app/src/lib/views/Add.svelte
@@ -1,27 +1,30 @@
 <script lang="ts">
-    import { link } from "svelte-spa-router";
+    import { link, push } from "svelte-spa-router";
     import AddCard from "../components/AddCard.svelte";
     import Logo from "../components/Logo.svelte";
+    import { routes } from "../../routes";
+    import axios from "axios";
 
     let delivery = {
-        product: '',
-        product_id: '',
-        sender: '',
-        receiver: '',
-        amount: 0
-    }
-    
+        product: "",
+        product_id: "",
+        sender: "",
+        receiver: "",
+        amount: 0,
+    };
+
     async function addDelivery() {
-        if (
-            Object.entries(delivery).every(
-                (value) => value != null && value != undefined
+        axios
+            .post(routes.add, delivery)
+            .then((res) =>
+                push("/search/" + res["data"]["product_id"]).then((_) =>
+                    alert("Encomenda adicionada com sucesso")
+                )
             )
-        ) {
-            await fetch("http://localhost:5000/delivery", {
-                method: 'POST',
-                body: JSON.stringify(delivery),
-            }).then((res) => console.log(res.body));
-        }
+            .catch((_) => {
+                alert("Não foi possível adicionar a encomenda");
+                push("/");
+            });
     }
 </script>
 
@@ -32,12 +35,17 @@
         class="text-sm self-start cursor-pointer hover:font-semibold"
         >Cancelar</a
     >
-    <Logo/>
-    <form name="add" class="w-full flex flex-col" on:submit|preventDefault={addDelivery}>
-    <AddCard {delivery} />
-    <input
-        type="submit"
-        value="Adicionar"
-        class="cursor-pointer hover:font-semibold self-end mt-6"/>
+    <Logo />
+    <form
+        name="add"
+        class="w-full flex flex-col"
+        on:submit|preventDefault={addDelivery}
+    >
+        <AddCard {delivery} />
+        <input
+            type="submit"
+            value="Adicionar"
+            class="cursor-pointer hover:font-semibold self-end mt-6"
+        />
     </form>
 </main>
